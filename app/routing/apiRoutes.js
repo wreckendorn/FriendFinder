25c671e07256db62ee457a670a133a3e252b5b6c
Hecky <thechrisheckendorn@gmail.com>
2018-07-06 17:26:52 -0500

    adding edits to allow input from user to post

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -2,3 +2,33 @@
 
 //    * A GET route with the url `/api/friends`. This will be used to display a JSON of all possible friends.
 //    * A POST routes `/api/friends`. This will be used to handle incoming survey results. This route will also be used to handle the compatibility logic. 
+
+// LOAD DATA
+var friends = require("../data/friends");
+
+module.exports = function(app) {
+
+
+    app.get("/api/friends", function(req, res) {
+        res.json(friends);
+        var getSum = 0;
+        var friendList = [];
+        // this loop goes through the friends objects from the JSON
+        for (var i = 0; i < res.length; i++) {
+            getSum = 0;
+            // this loop goes through the scores array and adds them together
+            for (var x = 0; x < data[i].scores.length; x++){
+                getSum += data[i].scores[x]
+            }
+            //adds the sum score to an array
+            friendList[i] = getSum;
+        }
+        return friendList;
+    });
+
+    app.post("/api/survey", function(req, res) {
+        
+        friends.push(req.body);
+        console.log("added to database");
+    });
+};
--- server.js
+++ server.js
@@ -1,13 +1,12 @@
 // DEPENDENCIES -----------------------
 var express = require('express');
 var bodyParser = require('body-parser');
-var path = require('path');
 //-------------------------------------
 
 //EXPRESS CONFIG -------------------------------------
 
 //creating express server
-var app = express;
+var app = express();
 
 //sets up port for listener
 var PORT = process.env.PORT || 8080;
@@ -18,8 +17,9 @@ app.use(bodyParser.json());
 //----------------------------------------------------
 
 //ROUTER ---------------------
-require("./routes/apiRoutes")(app);
-require("./routes/htmlRoutes")(app);
+require("./app/routing/apiRoutes")(app);
+require("./app/routing/htmlRoutes")(app);
+
 
 app.listen(PORT, function() {
     console.log("App listening on PORT: " + PORT);
